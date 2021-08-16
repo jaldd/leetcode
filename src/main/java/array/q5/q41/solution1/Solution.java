b550025fc6cca0765dde20264a4bddb75631867c
李丹丹(10037750) <lidd-h@glodon.com>
2021-08-16 15:56:17 +0800

    41

---
+++ src/main/java/array/q5/q41/solution1/Solution.java
@@ -0,0 +1,52 @@
+package array.q5.q41.solution1;
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = length + 1;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] <= length) {
+                nums[nums[i] - 1] = -nums[nums[i] - 1];
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return length + 1;
+
+
+//        int n = nums.length;
+//        for (int i = 0; i < n; ++i) {
+//            if (nums[i] <= 0) {
+//                nums[i] = n + 1;
+//            }
+//        }
+//        for (int i = 0; i < n; ++i) {
+//            int num = Math.abs(nums[i]);
+//            if (num <= n) {
+//                nums[num - 1] = -Math.abs(nums[num - 1]);
+
+//        nums[nums[i] - 1] = -nums[nums[i] - 1];
+//            }
+//        }
+//        for (int i = 0; i < n; ++i) {
+//            if (nums[i] > 0) {
+//                return i + 1;
+//            }
+//        }
+//        return n + 1;
+
+
+    }
+}
---
+++ src/test/java/array/q5/q41/solution2/TestSolution.java
@@ -0,0 +1,23 @@
+package array.q5.q41.solution2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSolution {
+
+    @Test
+    public void testRotate() {
+
+        int[] nums = new int[]{1, 2, -1, -4, 4, 6, 7};
+
+        int[] nums1 = new int[]{-1, -100, 1, 3};
+
+        Solution solution = new Solution();
+        int res = solution.firstMissingPositive(nums);
+        int res1 = solution.firstMissingPositive(nums1);
+        Assert.assertEquals(res, 3);
+        Assert.assertEquals(res1, 2);
+
+    }
+
+}
