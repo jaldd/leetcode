52c20e78d75582bd6b5dbe2e151de9691ec05cd7
李丹丹(10037750) <lidd-h@glodon.com>
2021-12-13 10:56:41 +0800

    q242 anagram

--- src/main/java/str/q8/qanagram/easy/q242/solution2/Solution.java
+++ src/main/java/str/q8/qanagram/easy/q242/solution2/Solution.java
@@ -1,5 +1,8 @@
 package str.q8.qanagram.easy.q242.solution2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public boolean isAnagram(String s, String t) {
@@ -22,4 +25,24 @@ public class Solution {
 
         return true;
     }
+
+
+//    public boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        Map<Character, Integer> table = new HashMap<Character, Integer>();
+//        for (int i = 0; i < s.length(); i++) {
+//            char ch = s.charAt(i);
+//            table.put(ch, table.getOrDefault(ch, 0) + 1);
+//        }
+//        for (int i = 0; i < t.length(); i++) {
+//            char ch = t.charAt(i);
+//            table.put(ch, table.getOrDefault(ch, 0) - 1);
+//            if (table.get(ch) < 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
