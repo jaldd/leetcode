585f0bc1c9ad9d14289498e466a67eb84071880c
lidd-h <lidd-h@glodon.com>
2025-09-01 18:27:24 +0800

    202509binary

--- src/main/java/l2025/l09/l01/Main.java
+++ src/main/java/l2025/l09/l01/Main.java
@@ -3,23 +3,35 @@ package l2025.l09.l01;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        
+
         // 测试用例1
         int[] nums1 = {2, 7, 11, 15};
         int target1 = 9;
         int[] result1 = solution.twoSum(nums1, target1);
         System.out.println("测试1结果: [" + result1[0] + ", " + result1[1] + "]");
-        
+
         // 测试用例2
         int[] nums2 = {3, 2, 4};
         int target2 = 6;
         int[] result2 = solution.twoSum(nums2, target2);
         System.out.println("测试2结果: [" + result2[0] + ", " + result2[1] + "]");
-        
+
         // 测试用例3
         int[] nums3 = {3, 3};
         int target3 = 6;
         int[] result3 = solution.twoSum(nums3, target3);
         System.out.println("测试3结果: [" + result3[0] + ", " + result3[1] + "]");
+    }
+
+    public boolean binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) return true;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
     }
 }
