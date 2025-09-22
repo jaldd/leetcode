e4c69730f833543c956834c846cae2cde991c346
lidd-h <lidd-h@glodon.com>
2025-09-22 17:15:00 +0800

    letcode7tian

--- src/main/java/l2025/l09/l07/Solution.java
+++ src/main/java/l2025/l09/l07/Solution.java
@@ -77,25 +77,23 @@ public class Solution {
         int m = text1.length();
         int n = text2.length();
 
-        // 只需要两行数组
-        int[] prev = new int[n + 1];
-        int[] curr = new int[n + 1];
+        int[] pre = new int[m + 1];
+        int[] cur = new int[n + 1];
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    curr[j] = prev[j - 1] + 1;
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    cur[j] = pre[j - 1] + 1;
                 } else {
-                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                    cur[j] = Math.max(pre[j], cur[j - 1]);
                 }
             }
-            // 交换数组
-            int[] temp = prev;
-            prev = curr;
-            curr = temp;
-        }
 
-        return prev[n];
+            int[] tmp = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre[n];
     }
 
     public static void main(String[] args) {
