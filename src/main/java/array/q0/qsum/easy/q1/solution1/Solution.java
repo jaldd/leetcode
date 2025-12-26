3b6c12a2edc2066f8215a0d8428fcabbb883003f
jaldd <806616508@qq.com>
2025-12-26 22:37:16 +0800

    commit

--- src/main/java/array/q0/qsum/easy/q1/solution1/Solution.java
+++ src/main/java/array/q0/qsum/easy/q1/solution1/Solution.java
@@ -1,15 +1,20 @@
 package array.q0.qsum.easy.q1.solution1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{nums[i], nums[j]};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int min = target - num;
+            if (map.containsKey(min)) {
+                return new int[]{map.get(min), i};
             }
+            map.put(num, i);
         }
-        return new int[0];
+        return new int[]{};
     }
 }
