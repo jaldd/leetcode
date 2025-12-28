f123fe43c1f7214378da4ef5984291b32957392f
jaldd <806616508@qq.com>
2025-12-28 21:16:32 +0800

    reverse

--- src/main/java/n2025/y12/ListNode.java
+++ src/main/java/n2025/y12/ListNode.java
@@ -47,4 +47,29 @@ public class ListNode {
         cur.next = list1 == null ? list2 : list1;
         return head.next;
     }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList1(head.next);
+        head.next.next = head;
+        head.next=null;
+        return newHead;
+    }
 }
