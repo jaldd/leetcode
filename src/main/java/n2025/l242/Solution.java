c39b0b054ae693cba7c6a7ea7609f8d663fff05e
jaldd <806616508@qq.com>
2026-01-10 20:52:17 +0800

    Anagram group

--- src/main/java/n2025/l242/Solution.java
+++ src/main/java/n2025/l242/Solution.java
@@ -1,7 +1,6 @@
 package n2025.l242;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public boolean isAnagram(String s, String t) {
@@ -44,4 +43,43 @@ public class Solution {
         }
         return map.isEmpty();
     }
+
+    public List<List<String>> groupAnagramsSort(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> orDefault = map.getOrDefault(key, new ArrayList<>());
+            orDefault.add(str);
+            map.put(key, orDefault);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagramsCount(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] counts = new int[26];
+            char[] chars = str.toCharArray();
+            for (char c : chars) {
+                counts[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            int iMax = counts.length - 1;
+            for (int i = 0; i < counts.length; i++) {
+                if (i == iMax) {
+                    sb.append(counts[i]);
+                    continue;
+                }
+                sb.append(counts[i]).append(",");
+            }
+            String key = sb.toString();
+            List<String> orDefault = map.getOrDefault(key, new ArrayList<>());
+            orDefault.add(str);
+            map.put(key, orDefault);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
