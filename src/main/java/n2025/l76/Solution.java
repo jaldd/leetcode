e890f68161e43ff50346a11f320cdf4bcac72f8c
jaldd <806616508@qq.com>
2026-01-21 22:38:50 +0800

    双指针

--- src/main/java/n2025/l76/Solution.java
+++ src/main/java/n2025/l76/Solution.java
@@ -150,4 +150,15 @@ public class Solution {
         }
         return new int[]{-1, -1};
     }
+
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
 }
