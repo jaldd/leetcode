0da70406a0a0eec7c7f395d53c61f1ae063bcff1
lidd-h <lidd-h@glodon.com>
2026-03-03 16:10:24 +0800

    l977

---
+++ src/main/java/l2026/l03/l977/Solution.java
@@ -0,0 +1,25 @@
+package l2026.l03.l977;
+
+public class Solution {
+
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+        int[] res = new int[n];
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                res[index]=leftSquare;
+                left++;
+            } else {
+                res[index]=rightSquare;
+                right--;
+            }
+            index--;
+        }
+        return res;
+    }
+}
